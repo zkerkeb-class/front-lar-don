71537784a3d2815ca5590903aae4d7d1a6142a9c
Aubin Olivrie <aubin.olivrie@smice.com>
2024-02-16 15:50:50 +0100

    [FIX] Async service

--- src/services/auth-service.jsx
+++ src/services/auth-service.jsx
@@ -4,19 +4,15 @@ const AuthenticationService = {
     return user !== null;
   },
   login: async (email, password) => {
-    new Promise((resolve, reject) => {
-      if (email === 'admin@mail.com' && password === 'admin') {
-        localStorage.setItem('user', JSON.stringify({ email, password }));
-        resolve(true);
-      }
-      reject(false);
-    });
+    if (email && password) {
+      localStorage.setItem('user', JSON.stringify({ email, password }));
+      return true;
+    }
+    return false;
   },
   logout: async () => {
-    new Promise((resolve, reject) => {
-      localStorage.removeItem('user');
-      resolve(true);
-    });
+    localStorage.removeItem('user');
+    return true;
   },
 };
 
